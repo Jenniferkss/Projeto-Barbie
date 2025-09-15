858aed4fc1e969851fec8ef738ce3a7c63dcf01b
jennifer <jennifer.k.silva7@aluno.senai.br>
2025-09-15 13:33:47 -0300

    feat: Atualiza arquivo projeto Barbie

--- src/controllers/barbieController.js
+++ src/controllers/barbieController.js
@@ -19,6 +19,65 @@ const getBarbiesById = (req,res)=> {
     })
 }
 
+const createBarbie = (req,res) => {
+    const {nome,profissao,anoLancamento} = req.body;
+
+    if(!nome || !profissao){
+        return res.status(400).json({
+            sucess: false, 
+            message: "Nome e profissão sao obrigatórios"
+        })
+    }
+
+    const novaBarbie = {
+    id: barbies.length +1,
+    nome: nome,
+    profissao: profissao,
+    anoLancamento: anoLancamento
+}
+
+barbies.push(novaBarbie);
+res.status(201).json({
+    sucess:true, 
+    message:"Barbie cadastrado com sucesso!",
+    barbie: novaBarbie
+})
+}
+
+const deleteBarbie = (req, res) => {
+    const { id } = req.params;
+
+    // Validar ID
+    if (isNaN(id)) {
+        return res.status(400).json({
+            success: false,
+            message: "ID deve ser um número válido!"
+        });
+    }
+
+    const idParaApagar = parseInt(id);
+    
+    // Verificar se bruxo existe antes de remover
+    const barbieParaRemover = barbies.find(b => b.id === idParaApagar);
+    if (!barbieParaRemover) {
+        return res.status(404).json({
+            success: false,
+            message: `Barbie com ID ${id} não encontrado para remoção!`
+        });
+    }
+
+    // Remover bruxo usando filter
+    const barbieFiltrado = barbies.filter(barbie => barbie.id !== idParaApagar);
+    
+    // Atualizar array global
+    barbies.splice(0, barbies.length, ...barbieFiltrado);
+
+    res.status(200).json({
+        success: true,
+        message: `Barbie ${barbieParaRemover.nome} (ID: ${id}) foi removido dos registros.`,
+        barbieRemovida: barbieParaRemover
+    });
+};
 
 
-export {getAllBarbies,getBarbiesById}
+export {getAllBarbies,getBarbiesById,createBarbie,deleteBarbie}
--- src/routes/barbieRoutes.js
+++ src/routes/barbieRoutes.js
@@ -1,13 +1,10 @@
 import express from "express";
-import {getAllBarbies, getBarbiesById} from "./../controllers/barbieController.js"
+import {getAllBarbies, getBarbiesById,createBarbie,deleteBarbie} from "./../controllers/barbieController.js"
 
 const router = express.Router();
 router.get("/",getAllBarbies);
 router.get("/:id",getBarbiesById);
-
-
-
-
-
+router.post("/",createBarbie);
+router.delete("/:id",deleteBarbie);
 
 export default router;
