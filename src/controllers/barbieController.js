02b37e72bfe81e1ad742238a2ff4a0ea1d51a453
jennifer <jennifer.k.silva7@aluno.senai.br>
2025-09-15 14:23:28 -0300

    feat: Atualiza arquivo projeto Barbie

--- src/controllers/barbieController.js
+++ src/controllers/barbieController.js
@@ -75,5 +75,35 @@ const deleteBarbie = (req, res) => {
     });
 };
 
+const updateBarbie = (req,res) => {
+    const id = parseInt(req.params.id);
+    const {nome,profissao,anoLancamento} = req.body
 
-export {getAllBarbies,getBarbiesById,createBarbie,deleteBarbie}
+    const idParaEditar = id; 
+
+    if(isNaN(idParaEditar)){
+        return res.status(404).json({
+            sucess: false,
+            message: `Barbie com Id: ${id} não existe`
+        })
+    }
+const barbiesAtualizadas = barbies.map(barbie => barbie.id === idParaEditar ? {
+    ...barbie,
+    ...(nome && {nome}),
+    ...(profissao && {profissao}),
+    ...(anoLancamento && {anoLancamento: parseInt(anoLancamento)})
+
+}: barbie) 
+
+barbies.splice(0,barbies.length,...barbiesAtualizadas);
+const barbieNova = barbies.find(barbie => barbie.id === idParaEditar);
+
+res.status(200).json({
+    sucess: true,
+    message: `Dados da Barbie ID ${idParaEditar} atualizados com sucesso!`,
+    barbie: barbieNova
+})
+}
+
+
+export {getAllBarbies,getBarbiesById,createBarbie,deleteBarbie,updateBarbie}
--- src/routes/barbieRoutes.js
+++ src/routes/barbieRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {getAllBarbies, getBarbiesById,createBarbie,deleteBarbie} from "./../controllers/barbieController.js"
+import {getAllBarbies, getBarbiesById,createBarbie,deleteBarbie, updateBarbie} from "./../controllers/barbieController.js"
 
 const router = express.Router();
 router.get("/",getAllBarbies);
 router.get("/:id",getBarbiesById);
 router.post("/",createBarbie);
 router.delete("/:id",deleteBarbie);
-
+router.put("/:id",updateBarbie);
 export default router;
